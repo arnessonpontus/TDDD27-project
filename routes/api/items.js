3a4dc207965a366ee81e134e4f53e85993b29b69
Pontus <ponar842@student.liu.se>
2020-04-17 09:55:29 +0200

    Added untracked model and route

---
+++ routes/api/items.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+// Item model
+const Item = require("../../models/Item");
+
+// @route  GET api/items
+// @desc   Get all Items
+// @access Public
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 }) // -1: decending
+    .then((items) => res.json(items));
+});
+
+// @route  POST api/items
+// @desc   post an Items
+// @access Public // Change to private after login
+router.post("/", (req, res) => {
+  const newItem = new Item({
+    name: req.body.name,
+  });
+
+  newItem.save().then((item) => res.json(item));
+});
+
+// @route  DELETE api/items:id
+// @desc   delete an Item
+// @access Public // Change to private after login
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then((item) => item.remove().then(() => res.json({ success: true })))
+    .catch((err) => res.status(404).json({ success: false })); // Sends 404 if item does not exist
+});
+
+module.exports = router;
