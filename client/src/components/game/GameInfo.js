3fc926d1fe2a6f2c9a752de45c5066260d732876
Pontus <ponar842@student.liu.se>
2020-06-01 20:36:58 +0200

    Small bug fix

--- client/src/components/game/GameInfo.js
+++ client/src/components/game/GameInfo.js
@@ -97,7 +97,7 @@ const GameInfo = (props) => {
         </p>
         <h1 className="is-size-7">Current category: </h1>
         <p className="is-size-6">{category ? category : "Not assigned"}</p>
-        {drawingWord && currentDrawer !== user.name ? (
+        {drawingWord && currentDrawer && currentDrawer.name === user.name ? (
           <Fragment>
             <h1 className="is-size-7">Draw the word: </h1>
             <p className="is-size-6">{drawingWord}</p>{" "}
